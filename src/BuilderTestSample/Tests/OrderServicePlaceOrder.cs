80df2bb3c3d9037ab77a42f746ae68f8c017ef00
Drew Buchanan <drewbuchanangmd@gmail.com>
2020-07-30 21:20:39 -0500

    Add ValidateOrder tests to their own class

--- src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
+++ src/BuilderTestSample/Tests/OrderServicePlaceOrder.cs
@@ -11,44 +11,47 @@ namespace BuilderTestSample.Tests
 		private readonly OrderService _orderService = new OrderService ();
 		private readonly OrderBuilder _orderBuilder = new OrderBuilder ();
 
-		[Fact]
-		public void ThrowsExceptionGivenOrderWithExistingId ()
+		public class OrderServiceValidateOrderMethod : OrderServicePlaceOrder
 		{
-			Order order = _orderBuilder
-							.WithTestValues ()
-							.WithId (123);
+			[Fact]
+			public void ThrowsExceptionGivenOrderWithExistingId ()
+			{
+				Order order = _orderBuilder
+								.WithTestValues ()
+								.WithId (123);
 
-			Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
-		}
+				Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
+			}
 
-		[Fact]
-		public void ThrowsExceptionGivenOrderWithNegativeAmount()
-		{
-			Order order = _orderBuilder
-				.WithTestValues ()
-				.WithAmount (-1);
+			[Fact]
+			public void ThrowsExceptionGivenOrderWithNegativeAmount()
+			{
+				Order order = _orderBuilder
+					.WithTestValues ()
+					.WithAmount (-1);
 
-			Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
-		}
+				Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
+			}
 
-		[Fact]
-		public void ThrowsExceptionGivenOrderWithZeroAmount ()
-		{
-			Order order = _orderBuilder
-				.WithTestValues ()
-				.WithAmount (0);
+			[Fact]
+			public void ThrowsExceptionGivenOrderWithZeroAmount ()
+			{
+				Order order = _orderBuilder
+					.WithTestValues ()
+					.WithAmount (0);
 
-			Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
-		}
+				Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
+			}
 
-		[Fact]
-		public void ThrowsExceptionGivenOrderWithNullCustomer()
-		{
-			Order order = _orderBuilder
-				.WithTestValues ()
-				.WithCustomer (null);
+			[Fact]
+			public void ThrowsExceptionGivenOrderWithNullCustomer()
+			{
+				Order order = _orderBuilder
+					.WithTestValues ()
+					.WithCustomer (null);
 
-			Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
+				Assert.Throws<InvalidOrderException> (() => _orderService.PlaceOrder (order));
+			}
 		}
 	}
 }
